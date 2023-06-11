0ba06de6d88337a4f0296417121411f7959fc6f8
Vrrrum <kamil2006wojtek@gmail.com>
2023-06-11 19:51:14 +0200

    Dodano stronę produktu

---
+++ src/php/product.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Szybka szama - Produkt</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/product.css">
+</head>
+<body>
+    <main>
+        <div class="banner"></div>
+
+        <h1>Pizza italiana peperoni</h1>
+        <p>34.99zł</p>
+        <div>
+            <img width="60" src="../img/add.svg" alt="">
+            <img width="60" src="../img/fav.svg" alt="">
+        </div>
+
+        <h2>Informacje</h2>
+        <h3>Alergeny</h3>
+        <ul>
+            <li>Orzeszki ziemne</li>
+            <li>Mięczaki</li>
+        </ul>
+        <h3>Wartość kaloryczna <sup>Całość</sup></h3>
+        <div class="cal-bar">
+            <div style="width: 50%"></div>
+        </div>
+        <p>1044 kcl / 2500 kcl dziennie</p>
+
+        <h3>Wartość kaloryczna <sup>Kawałek</sup></h3>
+        <div class="cal-bar">
+            <div style="width: 10%"></div>
+        </div>
+        <p>233 kcl / 2500 kcl dziennie</p>
+    </main>
+
+
+</body>
+</html>
